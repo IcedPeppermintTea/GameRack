682fc88be57cb1d181cb7863e5779eb6fd41265a
IcedPeppermintTea <238953108+IcedPeppermintTea@users.noreply.github.com>
2026-03-19 22:00:42 -0700

    initial commit

---
+++ schema.sql
@@ -0,0 +1,26 @@
+-- tables necessary for the gamesrack.db database 
+
+CREATE TABLE users (
+id INTEGER PRIMARY KEY,
+username TEXT UNIQUE NOT NULL,
+password TEXT NOT NULL
+);
+
+CREATE TABLE games (
+id INTEGER PRIMARY KEY,
+rawg_id INTEGER UNIQUE,
+title TEXT NOT NULL,
+cover_url TEXT
+);
+
+CREATE TABLE library (
+id INTEGER PRIMARY KEY,
+user_id INTEGER NOT NULL,
+game_id INTEGER NOT NULL,
+state TEXT NOT NULL,
+rating INTEGER,
+review TEXT,
+date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY(user_id) REFERENCES users(id),
+FOREIGN KEY(game_id) REFERENCES games(id)
+);
