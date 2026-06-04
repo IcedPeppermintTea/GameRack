e8b7ed20ff4053d8231fa2d495d276d93a4735c4
IcedPeppermintTea <238953108+IcedPeppermintTea@users.noreply.github.com>
2026-06-03 22:46:41 -0700

    login ui complete

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ bcrypt = Bcrypt(app)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    # variables
+    error_msg = "" 
+
     if request.method == "POST":
         # validate login
         username = request.form.get("username")
@@ -28,17 +31,16 @@ def index():
 
         # does username exist in the db
         if db_match is None:
-            return render_template("error.html", error="The Username entered matches no accounts")
+            error_msg="The Username entered matches no accounts. Please try again."
         else:
-            db_password = db_match[1]
             is_pswd_valid = bcrypt.check_password_hash(db_match[1], password)
             if (is_pswd_valid):
                 session["username"] = username
                 return redirect(url_for("home"))
             else:
-                return render_template("error.html", error="The Password entered is incorrect")
-    else:
-        return render_template("index.html")
+                error_msg="The Password entered is incorrect"
+    
+    return render_template("index.html", error_msg=error_msg)
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
@@ -49,7 +51,7 @@ def signup():
 
         # validate form values exist
         if not request.form.get("username") or not request.form.get("password") or not request.form.get("confirmed_password"):
-            return render_template("error.html", error="Invalid Form Submission")
+            return render_template("error.html", error="Invalid Form Submission"), 400
         else:
             # validate username is not taken
 
@@ -69,16 +71,17 @@ def signup():
                     
                     session["username"] = username
 
-                    return redirect(url_for("home"))  
+                    # 303 - code after successful POST that redirects to a GET
+                    return redirect(url_for("home"), 303)  
 
                 else:
-                    return render_template("error.html", error="Password does not match, please try again")
+                    return render_template("error.html", error="Password does not match, please try again"), 422
 
             else:
-                return render_template("error.html", error="Password does not meet minimal requirements")
+                return render_template("error.html", error="Password does not meet minimal requirements"), 422
 
     else:
-        return render_template("signup.html")
+        return render_template("signup.html"), 200
 
 @app.route("/home")
 def home():
