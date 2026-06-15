35e270c3357a61f15258032bfa1bca8670885281
IcedPeppermintTea <238953108+IcedPeppermintTea@users.noreply.github.com>
2026-06-14 21:35:09 -0700

    toast message for game addition

--- static/js/add-game.js
+++ static/js/add-game.js
@@ -3,6 +3,7 @@
 document.addEventListener("DOMContentLoaded", function() {  
 
     const resultsContainer = document.querySelector("#search-results");
+    const gameAddedMsg = document.querySelector("#game-added");
 
     // on 'Add to Shelf' button click 
     resultsContainer.addEventListener("click", async e=> {
@@ -13,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
             const gameRawgId = e.target.getAttribute("data-id");
             const gameTitle = e.target.getAttribute("data-title");
             const gameCoverImg = e.target.getAttribute("data-cover");
+
 
             // send Flask POST request
             const response = await fetch("/library/add", {
@@ -30,8 +32,23 @@ document.addEventListener("DOMContentLoaded", function() {
             // open the envelop
             const data = await response.json()
 
-            // console log for now
-            console.log(data);
+            // check if response is successful
+            if (data["success"] == true) {
+                gameAddedMsg.innerHTML = `<p class="toast-msg">Game Added Successfully</p>`
+
+                // hide it after 3 seconds
+                setTimeout(() => {
+                gameAddedMsg.innerHTML = ""
+                }, 3000)
+            }
+            else {
+                gameAddedMsg.innerHTML = `<p class="toast-msg">Something went wrong, please try again</p>`
+
+                // hide it after 3 seconds
+                setTimeout(() => {
+                gameAddedMsg.innerHTML = ""
+                }, 3000)
+            }
         }
     })
 });
