02066df2d2393c3fa3af3dac2ce6bf4632811def
IcedPeppermintTea <238953108+IcedPeppermintTea@users.noreply.github.com>
2026-06-16 22:40:21 -0700

    WIP: save edit requests

--- static/js/library.js
+++ static/js/library.js
@@ -15,12 +15,37 @@ document.addEventListener("DOMContentLoaded", function () {
       card.addEventListener("click", (e) => {
         // show modal
         gameModal.classList.toggle("disp-none");
+
+        // get game details from library.html
+        const title = card.dataset.title;
+        const cover = card.dataset.cover;
+        const state = card.dataset.state;
+        const rating = card.dataset.rating;
+        const review = card.dataset.review;
+
+        // populate game details in the modal
+        document.querySelector("#modal-cover").src = cover;
+        document.querySelector("#modal-cover").alt = title;
+        document.querySelector("#modal-title").textContent = title;
+        document.querySelector("#modal-status").value = state;
       });
     });
   });
+
+  /* Handle saving changes */
+  const SaveBtn = document.querySelector("#modal-save");
+
+  SaveBtn.addEventListener("click", () => {
+    // store the values
+    const updatedStatus = document.querySelector("#modal-status").value;
+    const updatedRating = document.querySelector("#modal-rating").value;
+    const updatedReview = document.querySelector("#modal-review").value;
+
+    // send them in fetch request
+  });
+
   /* Handle exit button*/
   const closeBtn = document.querySelector("#modal-close");
-
   closeBtn.addEventListener("click", () => {
     gameModal.classList.toggle("disp-none");
   });
